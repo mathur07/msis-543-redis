c067d9c35b483f8dd92da9fef4bacaf5ac7984c3
Shubham Mathur <shubham07mathur@gmail.com>
2022-11-28 12:42:28 -0800

    009 code cleanup

--- nodeServer.js
+++ nodeServer.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const Redis = require('redis')
-const { json, response } = require('express')
-
 
 const app = express()
 app.use(cors())
@@ -21,7 +19,6 @@ app.get('/', async (req, res) => {
     }
     res.send('Hello World!')
 })
-
 
 app.get("/photos", async (req, res) => {
     try {
@@ -46,54 +43,7 @@ app.get("/photos", async (req, res) => {
             }
         })
         .catch(err => console.log(err))
-
-    // await redisClient.get('photos', async (err, photos) => {
-    //     if (err) {
-    //         console.error(err);
-    //     }
-    //     if (photos != null) {
-    //         console.log("Found in redis cache");
-    //         return res.json(JSON.parse(photos))
-    //     } else {
-    //         fetch("https://jsonplaceholder.typicode.com/photos")
-    //             .then((response) => response.json())
-    //             .then((data) => {
-    //                 redisClient.set('photos', JSON.stringify(data))
-    //                 res.json(data)
-    //             })
-    //             .catch(function (err) {
-    //                 console.log("Unable to fetch -", err);
-    //             });
-    //     }
-    // })
 })
-
-
-// app.get("/photos" , async(req,res)=>{
-//     redisClient.get('photos',async (err,photos)=>{
-//         if (err) console.error(err)
-//         if (photos != null) {
-//             return res.json(JSON.parse(photos))
-//         } else {
-//             const albumId = req.query.albumId
-//             const {data} = await axios.get(
-//             "https://jsonplaceholder.typicode.com/photos",
-//             {params:{albumId}}
-//     )
-//             redisClient.setEx('photos',REDIS_EXP,JSON.stringify(data))
-//         }
-//         res.json(data)
-//     })
-// })
-
-// app.get("/photos/:id", async (req, res) => {
-//     const { data } = await axios.get(
-//         `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
-//     )
-//     console.log(data);
-//     res.send(data)
-//     // .json(JSON.stringify(data))
-// })
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
