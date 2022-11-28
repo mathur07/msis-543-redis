9d2c5267c7d885879dfa6808849da3b3d90962fa
Shubham Mathur <shubham07mathur@gmail.com>
2022-11-28 12:40:56 -0800

    008 everything seems to be working

--- nodeServer.js
+++ nodeServer.js
@@ -17,9 +17,8 @@ app.get('/', async (req, res) => {
     try {
         await redisClient.connect()
     } catch (error) {
-        console.log("already redisClient is connected");
+        console.log("redisClient is connected " + error);
     }
-    await redisClient.set("hello", "World")
     res.send('Hello World!')
 })
 
@@ -28,40 +27,45 @@ app.get("/photos", async (req, res) => {
     try {
         await redisClient.connect()
     } catch (error) {
-        console.log("already redisClient is connected");
+        console.log("redisClient is connected " + error);
     }
-
-    redisClient.get('photos', async (err, photos)=>{
-        if(err){
-            console.log(err);
-        }
-        if (photos) {
-            
-        }
-    })
-    fetch("https://jsonplaceholder.typicode.com/photos")
-        .then((response) => response.json())
-        .then((data) => {
-            redisClient.set('photos', JSON.stringify(data))
-            res.json(data)
+    redisClient.get("photos")
+        .then(val => {
+            if (val != null) {
+                res.json(JSON.parse(val))
+            } else {
+                fetch("https://jsonplaceholder.typicode.com/photos")
+                    .then((response) => response.json())
+                    .then((data) => {
+                        redisClient.set('photos', JSON.stringify(data))
+                        res.json(data)
+                    })
+                    .catch(function (err) {
+                        console.log("Unable to fetch -", err);
+                    });
+            }
         })
-        .catch(function (err) {
-            console.log("Unable to fetch -", err);
-        });
-    // const albumId = req.query.albumId
-    // await axios.get(
-    //     "https://jsonplaceholder.typicode.com/photos",
-    //     {params: {albumId}}
-    // )
-    // .then((response)=>{
-    //     console.log(response.data);
-    //     console.log(response.status);
-    //     console.log(response.statusText);
-    //     console.log(response.headers);
-    //     console.log(response.config);
+        .catch(err => console.log(err))
+
+    // await redisClient.get('photos', async (err, photos) => {
+    //     if (err) {
+    //         console.error(err);
+    //     }
+    //     if (photos != null) {
+    //         console.log("Found in redis cache");
+    //         return res.json(JSON.parse(photos))
+    //     } else {
+    //         fetch("https://jsonplaceholder.typicode.com/photos")
+    //             .then((response) => response.json())
+    //             .then((data) => {
+    //                 redisClient.set('photos', JSON.stringify(data))
+    //                 res.json(data)
+    //             })
+    //             .catch(function (err) {
+    //                 console.log("Unable to fetch -", err);
+    //             });
+    //     }
     // })
-    // // redisClient.set("photos",REDIS_EXP, JSON.stringify(data))
-    // res.json(data)
 })
 
 
@@ -82,14 +86,14 @@ app.get("/photos", async (req, res) => {
 //     })
 // })
 
-app.get("/photos/:id", async (req, res) => {
-    const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
-    )
-    console.log(data);
-    res.send(data)
-    // .json(JSON.stringify(data))
-})
+// app.get("/photos/:id", async (req, res) => {
+//     const { data } = await axios.get(
+//         `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+//     )
+//     console.log(data);
+//     res.send(data)
+//     // .json(JSON.stringify(data))
+// })
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
